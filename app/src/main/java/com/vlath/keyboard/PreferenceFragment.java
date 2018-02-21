436719fbab834e7b7b045be1cfe7e728d9b069f0
Tim Krief <tim.krief@gmail.com>
2018-02-22 00:00:33 +0100

    Add files via upload
    "Qwerty" layout is now "standard" layout, and a new "layout" option in the preference page allow the user to chose a local layout (qwerty or azerty for the moment). The way I implemented it is really flexible, making it easy to add more local layouts in the future.

--- app/src/main/java/com/vlath/keyboard/PreferenceFragment.java
+++ app/src/main/java/com/vlath/keyboard/PreferenceFragment.java
@@ -13,14 +13,17 @@ public class PreferenceFragment extends android.preference.PreferenceFragment im
 
     ListPreference listTheme;
     ListPreference listStart;
+    ListPreference listLayout;
     @Override
     public void onCreate(Bundle s){
         super.onCreate(s);
         addPreferencesFromResource(R.xml.ime_preferences);
         listTheme = (ListPreference) findPreference("theme");
         listStart = (ListPreference) findPreference("start");
+        listLayout = (ListPreference) findPreference("layout");
         listTheme.setSummary(listTheme.getEntry());
         listStart.setSummary(listStart.getEntry());
+        listLayout.setSummary(listLayout.getEntry());
         PreferenceManager.getDefaultSharedPreferences(getActivity().getBaseContext()).registerOnSharedPreferenceChangeListener(this);
     }
 
@@ -29,5 +32,6 @@ public class PreferenceFragment extends android.preference.PreferenceFragment im
     public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String s) {
         listTheme.setSummary(listTheme.getEntry());
         listStart.setSummary(listStart.getEntry());
+        listLayout.setSummary(listLayout.getEntry());
     }
 }
